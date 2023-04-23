3ccec236713f6230dc73d984b6ecd397a9c85af2
W. Augusto Andreoli <andreoliwa@proton.me>
2023-04-23 19:14:13 +0200

    refactor: slightly reduce complexity

--- src/logseq_doctor/api.py
+++ src/logseq_doctor/api.py
@@ -125,7 +125,7 @@ class Page:
             file.write(Block.indent(new_text, level) + LINE_BREAK)
             file.write(remaining_content)
 
-    def find_slice(  # noqa: C901 # FIXME[AA]: refactor this method
+    def find_slice(
         self,
         search_string: str,
         *,
@@ -134,65 +134,66 @@ class Page:
         level: int = None,
     ) -> Optional[Slice]:
         """Find a slice of Markdown blocks in a Logseq page."""
-        if not self.path.exists():
+        try:
+            # TODO: The right way would be to navigate the Markdown AST.
+            #  This is a hacky/buggy solution that works for now, for most cases.
+            content = self.path.read_text()
+            relative_content = content[start:] if end is None else content[start:end]
+            pos_search_string = relative_content.find(search_string)
+            if pos_search_string == -1:
+                return None
+
+            previous_line_break = self._find_previous_line_break(relative_content, pos_search_string, level)
+            slice_start = previous_line_break + 1
+
+            column = relative_content[slice_start:].find(DASH)
+            if column == -1:
+                column = 0  # TODO: test this case
+
+            spaces = LINE_BREAK + (SPACE * column)
+            spaces_with_dash = spaces + DASH
+
+            pos_last_line = relative_content[slice_start:].find(spaces_with_dash)
+            if pos_last_line == -1:
+                pos_last_line = 0
+                while True:
+                    pos_next = relative_content[slice_start + pos_last_line :].find(spaces)
+                    if pos_next == -1:
+                        break
+                    pos_last_line += pos_next + 1
+
+            pos_next_line_break = relative_content[slice_start + pos_last_line :].find(LINE_BREAK)
+            # TODO: test: file without line break at the end
+            slice_end = (
+                len(relative_content)
+                if pos_next_line_break == -1
+                else slice_start + pos_last_line + pos_next_line_break + 1
+            )
+
+            if end is not None and slice_end > end:
+                return None
+
+            return Slice(
+                content=str(content[slice_start + start : slice_end + start]),
+                start_index=slice_start + start,
+                end_index=slice_end + start,
+            )
+        except (FileNotFoundError, ValueError):
             return None
 
-        # TODO: The right way would be to navigate the Markdown AST.
-        #  This is a hacky/buggy solution that works for now, for most cases.
-        content = self.path.read_text()
-        relative_content = content[start:] if end is None else content[start:end]
-        pos_search_string = relative_content.find(search_string)
-        if pos_search_string == -1:
-            return None
-
+    @staticmethod
+    def _find_previous_line_break(relative_content: str, pos_search_string: int, level: int = None) -> int:
         if level is None:
             bullet = DASH + SPACE
             previous_dash = relative_content[:pos_search_string].rfind(bullet)
             if previous_dash == -1:
-                return None  # TODO: test: no outline before search string
+                msg = "No bullet found before search string"
+                raise ValueError(msg)
 
-            previous_line_break = relative_content[:previous_dash].rfind(LINE_BREAK)
+            return relative_content[:previous_dash].rfind(LINE_BREAK)
             # There are no line breaks before on the first line of the file
-            slice_start = 0 if previous_line_break == -1 else previous_line_break + 1
-        else:
-            bullet = LINE_BREAK + (SPACE * (level * 2)) + DASH + SPACE
-            previous_line_break = relative_content[:pos_search_string].rfind(bullet)
-            if previous_line_break == -1:
-                return None  # TODO: test this case
-            slice_start = previous_line_break + 1
-
-        column = relative_content[slice_start:].find(DASH)
-        if column == -1:
-            column = 0  # TODO: test this case
-
-        spaces = LINE_BREAK + (SPACE * column)
-        spaces_with_dash = spaces + DASH
-
-        pos_last_line = relative_content[slice_start:].find(spaces_with_dash)
-        if pos_last_line == -1:
-            pos_last_line = 0
-            while True:
-                pos_next = relative_content[slice_start + pos_last_line :].find(spaces)
-                if pos_next == -1:
-                    break
-                pos_last_line += pos_next + 1
-
-        pos_next_line_break = relative_content[slice_start + pos_last_line :].find(LINE_BREAK)
-        # TODO: test: file without line break at the end
-        slice_end = (
-            len(relative_content)
-            if pos_next_line_break == -1
-            else slice_start + pos_last_line + pos_next_line_break + 1
-        )
-
-        if end is not None and slice_end > end:
-            return None
-
-        return Slice(
-            content=str(content[slice_start + start : slice_end + start]),
-            start_index=slice_start + start,
-            end_index=slice_end + start,
-        )
+        bullet = LINE_BREAK + (SPACE * (level * 2)) + DASH + SPACE
+        return relative_content[:pos_search_string].rfind(bullet)
 
 
 @dataclass
