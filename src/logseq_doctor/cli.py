abde0d7e22abd912ea0340bf9dffef0ef5df3d9d
W. Augusto Andreoli <andreoliwa@proton.me>
2024-01-09 22:42:32 +0100

    fix: remove --format kanban
    The Kanban plugin has queries for a while now

--- src/logseq_doctor/cli.py
+++ src/logseq_doctor/cli.py
@@ -23,7 +23,7 @@ from typing import List
 import typer
 
 from logseq_doctor import flat_markdown_to_outline
-from logseq_doctor.api import Block, Kanban, Logseq, Page
+from logseq_doctor.api import Block, Logseq
 
 app = typer.Typer(no_args_is_help=True)
 
@@ -63,7 +63,6 @@ class TaskFormat(str, Enum):
     """Task format."""
 
     text = "text"
-    kanban = "kanban"
 
 
 @app.command()
@@ -80,40 +79,9 @@ def tasks(
         dir_okay=True,
         file_okay=False,
     ),
-    format_: TaskFormat = typer.Option(
-        TaskFormat.text,
-        "--format",
-        "--pretty",
-        "-f",
-        help="Output format",
-        case_sensitive=False,
-    ),
-    output_path: Path = typer.Option(
-        None,
-        "--output",
-        "-o",
-        help="Output path (used only on the Kanban output format)",
-        file_okay=True,
-        dir_okay=False,
-        writable=True,
-    ),
 ) -> None:
     """List tasks in Logseq."""
     logseq = Logseq(logseq_host_url, logseq_api_token, logseq_graph_path)
-    if format_ == TaskFormat.kanban:
-        if not output_path and len(tag_or_page) == 1:
-            first_tag = tag_or_page[0]
-            page = logseq.page_from_name(first_tag)
-            output_path = page.path
-            typer.echo(f"Assuming Kanban file from tag/page name ({first_tag!r})")
-
-        if not output_path:
-            typer.secho(
-                "Kanban format requires an output path when multiple (or no) tags or pages are provided",
-                fg=typer.colors.BRIGHT_RED,
-            )
-            raise typer.Exit(1)
-
     condition = ""
     if tag_or_page:
         if len(tag_or_page) == 1:
@@ -124,30 +92,6 @@ def tasks(
     query = f"(and{condition} (task TODO DOING WAITING NOW LATER))"
 
     blocks_sorted_by_date = Block.sort_by_date(logseq.query(query))
-
-    if format_ == TaskFormat.kanban:
-        page = Page(output_path)
-        page.add_line_break()
-
-        kanban = Kanban(page, blocks_sorted_by_date)
-        typer.echo("Page URL: ", nl=False)
-        typer.secho(page.url(logseq.graph_name), fg=typer.colors.BLUE, bold=True)
-        if kanban.find():
-            typer.echo(f"Kanban board being updated at {output_path}")
-            kanban.update()
-        else:
-            typer.echo(f"Kanban board being added to {output_path}")
-            try:
-                kanban.add()
-            except FileNotFoundError as err:
-                typer.secho(str(err), fg=typer.colors.BRIGHT_RED)
-                typer.secho("Add some content to the page and try again: ", fg=typer.colors.BRIGHT_RED, nl=False)
-                typer.secho(page.url(logseq.graph_name), fg=typer.colors.BLUE, bold=True)
-                raise typer.Exit(1) from err
-
-        page.remove_line_break()
-        return
-
     for block in blocks_sorted_by_date:
         typer.secho(f"{block.page_title}: ", fg=typer.colors.GREEN, nl=False)
         typer.secho(block.url(logseq.graph_name), fg=typer.colors.BLUE, bold=True, nl=False)
