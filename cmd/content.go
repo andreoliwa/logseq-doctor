6b4b6c707dfa8c151e8c0e1227e7347f6042408e
W. Augusto Andreoli <andreoliwa@sent.com>
2025-07-07 23:06:30 +0200

    feat(content): add to the desired journal date

--- cmd/content.go
+++ cmd/content.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
-	"github.com/andreoliwa/lsd/internal"
 	"log"
 	"os"
 	"time"
 
+	"github.com/andreoliwa/lsd/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -20,14 +20,29 @@ For now, it will be appended at the end of the current journal page.`,
 	Run: func(_ *cobra.Command, _ []string) {
 		graph := internal.OpenGraphFromPath(os.Getenv("LOGSEQ_GRAPH_PATH"))
 		stdin := internal.ReadFromStdin()
-		_, err := internal.AppendRawMarkdownToJournal(graph, time.Now(), stdin)
+
+		var targetDate time.Time
+		if journalFlag != "" {
+			parsedDate, err := time.Parse("2006-01-02", journalFlag)
+			if err != nil {
+				log.Fatalln("Invalid journal date format. Use YYYY-MM-DD:", err)
+			}
+			targetDate = parsedDate
+		} else {
+			targetDate = time.Now()
+		}
+
+		_, err := internal.AppendRawMarkdownToJournal(graph, targetDate, stdin)
 		if err != nil {
 			log.Fatalln(err)
 		}
 	},
 }
 
+var journalFlag string //nolint:gochecknoglobals
+
 func init() {
 	// TODO: Future flags for this command could be --append (the default when not informed) and --prepend.
+	contentCmd.Flags().StringVarP(&journalFlag, "journal", "j", "", "Journal date in YYYY-MM-DD format (default: today)")
 	rootCmd.AddCommand(contentCmd)
 }
