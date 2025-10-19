0c9b2e9d03190352a3016088248fe26d0d77657b
W. Augusto Andreoli <andreoliwa@sent.com>
2025-10-19 13:21:33 +0200

    fix(md): rename --parent flag to --block

--- cmd/md.go
+++ cmd/md.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -30,7 +29,7 @@ func NewMdCmd(deps *MdDependencies) *cobra.Command {
 		}
 	}
 
-	var journalFlag, parentFlag string
+	var journalFlag, blockFlag string
 
 	cmd := &cobra.Command{ //nolint:exhaustruct
 		Use:   "md",
@@ -38,44 +37,37 @@ func NewMdCmd(deps *MdDependencies) *cobra.Command {
 		Long: `Add Markdown content to Logseq using the DOM.
 
 Pipe your Markdown content via stdin.
-If --parent is provided, the content will be added as a child block under the first block
+If --block is provided, the content will be added as a child block under the first block
 containing the specified text. Otherwise, it will be appended at the end of the journal page.
 
 Examples:
   echo "New task" | lsd md
-  echo "Child task" | lsd md --parent "Project A"
-  echo "Another task" | lsd md -p "meeting notes"`,
+  echo "Child task" | lsd md --block "Project A"
+  echo "Another task" | lsd md -b "meeting notes"`,
 		RunE: func(_ *cobra.Command, _ []string) error {
 			graphPath := os.Getenv("LOGSEQ_GRAPH_PATH")
 			stdin := deps.ReadStdin()
 			graph := deps.OpenGraph(graphPath)
 
-			var targetDate time.Time
-			if journalFlag != "" {
-				parsedDate, err := time.Parse("2006-01-02", journalFlag)
-				if err != nil {
-					return fmt.Errorf("invalid journal date format. Use YYYY-MM-DD: %w", err)
-				}
-
-				targetDate = parsedDate
-			} else {
-				targetDate = deps.TimeNow()
+			targetDate, err := ParseDateFromJournalFlag(journalFlag, deps.TimeNow)
+			if err != nil {
+				return err
 			}
 
 			opts := &internal.InsertMarkdownOptions{
 				Graph:      graph,
 				Date:       targetDate,
 				Content:    stdin,
-				ParentText: parentFlag,
+				ParentText: blockFlag,
 			}
 
 			return deps.InsertFn(opts)
 		},
 	}
 
-	cmd.Flags().StringVarP(&journalFlag, "journal", "j", "", "Journal date in YYYY-MM-DD format (default: today)")
-	cmd.Flags().StringVarP(&parentFlag, "parent", "p", "",
-		"Partial text of a block that will be the parent of the added Markdown")
+	addJournalFlag(cmd, &journalFlag)
+	addBlockFlag(cmd, &blockFlag, "Markdown content")
+	// TODO: addPageFlag(cmd, &pageFlag, "Markdown content")
 
 	return cmd
 }
